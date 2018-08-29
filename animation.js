4c533e4891b9edacabc7b25835fb40d9ed3b5db2
HamzaJirah <hamzajirah@gmail.com>
2018-08-29 18:56:05 +0100

    add setTimeOut

--- animation.js
+++ animation.js
@@ -2,6 +2,9 @@ function animateIcon(){
   let fist = document.querySelector("#fist")
   fist.innerHTML = "&#xf17a;";
   //add another icon
+  setTimeout(function(){
+    fist.innerHTML = "&#xf257;";
+  }, 1000);
 }
 //run function
 animateIcon();
