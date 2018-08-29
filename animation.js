bb2296f008475eaf1234e8f8cd8d07cdd4500ba1
HamzaJirah <hamzajirah@gmail.com>
2018-08-29 18:30:12 +0100

    created a setTimeOut function

--- animation.js
+++ animation.js
@@ -2,6 +2,9 @@ function animateIcon(){
   let fist = document.querySelector("fist")
   fist.innerHTML = "&#xf207;";
   //add another icon
+  setTimeout(function(){
+    
+  })
 }
 //run function
 animateIcon();
